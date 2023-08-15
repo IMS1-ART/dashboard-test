12a0a1d016491c104c1620be2e07c836093c0e33
IMS1-ART <141460757+IMS1-ART@users.noreply.github.com>
2023-08-15 19:09:53 +0100

    Update Header.js

--- src/components/Header.js
+++ src/components/Header.js
@@ -10,16 +10,6 @@ const page = ({ f_name, activePage }) => {
     <main className="bg-[#fff] flex-1">
       <header className="header flex flex-row w-full columns-1 bg-black justify-start mb-2 h-[80px]">
         <Link className="items-center justify-center flex" href={"/"}>
-    {/* <Image
-            onClick={() => {
-              window.location.href = "/home";
-            }}
-            // style={{}}
-            src={"/ims.png"}
-            width={90}
-            className="resize m-2 ml-4"
-            height={15}
-          /> */}
         </Link>
         <div
           onClick={() => setToggleMenu(true)}
